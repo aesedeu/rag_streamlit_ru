2254fb7408d14eb2718582124cfa9938cf5ddf17
Eugene Chernov <ev7080@yandex.ru>
2024-03-18 14:45:00 +0300

    changes in start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ echo SOURCE_DOCUMENTS_FOLDER=SOURCE_DOCUMENTS >> .env
 
 mkdir $SOURCE_DOCUMENTS_FOLDER
 mkdir logs
+mkdir dialogs
 
 echo "========================================="
 echo "Creating vectorstore ChromaDB..."
@@ -17,7 +18,6 @@ python3.10 -m venv .venv
 echo "$(pwd)" > .venv/lib/python3.10/site-packages/my_project.pth 
 . .venv/bin/activate
 pip install -r requirements.txt
-python --version
 
 # Running main app
 sh app.sh
